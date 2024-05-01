a76ce12e1607c8d1e2df15b44996e2c8b6f03562
Mattias Andersson <mattias800@gmail.com>
2024-05-01 10:48:05 +0200

    Cleanup and rename

---
+++ game/world/world01/level01/room_01.gd
@@ -0,0 +1,10 @@
+extends TileMap
+
+var has_entered_room := false
+
+func on_player_enter_room():
+	if not has_entered_room:
+		CutsceneManager.start_timeline("level0start")
+		await Dialogic.timeline_ended
+		
+	has_entered_room = true
