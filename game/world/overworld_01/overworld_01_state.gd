8f1bab797a4f709bb08848ac22addcf3bce996a7
Mattias Andersson <mattias800@gmail.com>
2024-05-05 11:42:23 +0200

    - Remove some unused files. - Add more collision shapes to overworld.

--- game/world/overworld_01/overworld_01_state.gd
+++
@@ -1,5 +0,0 @@
-class_name Overworld01State
-
-var level01: Level01State = Level01State.new()
-
-var has_ever_visited: bool = false
--- game/world/world_01/level_01/level_01_state.gd
+++
@@ -1,3 +0,0 @@
-class_name Level01State
-
-var has_ever_visited: bool = false
