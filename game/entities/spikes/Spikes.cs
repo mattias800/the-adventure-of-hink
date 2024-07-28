1f78541e3990a9c7ea558e5a27118fbe92c03c8a
Mattias Andersson <mattias800@gmail.com>
2024-07-28 17:05:24 +0200

    - Migrate spikes.

---
+++ game/entities/spikes/Spikes.cs
@@ -0,0 +1,21 @@
+using Godot;
+using Theadventureofhink.autoloads;
+using Theadventureofhink.utils;
+
+public partial class Spikes : Sprite2D
+{
+    private GameManager _gameManager;
+
+    public override void _Ready()
+    {
+        _gameManager = GetNode<GameManager>(Singletons.GameManager);
+    }
+
+    public void OnArea2dBodyEntered(Node2D body)
+    {
+        if (CollisionUtil.IsPlayer(body))
+        {
+            CallDeferred(nameof(_gameManager.RespawnPlayer));
+        }
+    }
+}
--- game/entities/spikes/spikes.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _on_area_2d_body_entered(body):
-	if body.is_in_group("player"):
-		GameManager.respawn_player.call_deferred()
