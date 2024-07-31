712bb87b54585618fa760939ce0e77a2a997c0ef
Mattias Andersson <mattias800@gmail.com>
2024-07-31 19:31:53 +0200

    - Trying to integrate dialogues and game state.

--- game/src/game_state/dialogue_state.gd
+++ game/src/game_state/dialogue_state.gd
@@ -1,3 +1,16 @@
 extends Node
 
-var mushy : Dictionary = {}
+var grandpa = {}
+var mushy = {}
+
+func player_can_double_jump():
+	print("GameState")
+	print(str(GameState))
+	return GameState.PlayerState.PlayerSkillsState.CanDoubleJump.Value()
+	
+func player_can_wall_jump():
+	return true
+func player_can_climb_walls():
+	return true
+func player_can_dash():
+	return true
