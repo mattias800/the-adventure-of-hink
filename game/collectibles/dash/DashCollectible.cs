dda27f75441a6be8ff1a38830ff096fbd75bff48
Mattias Andersson <mattias800@gmail.com>
2024-09-08 14:32:31 +0200

    - Sparkly - More dialogue - Flickering light

--- game/collectibles/dash/DashCollectible.cs
+++ game/collectibles/dash/DashCollectible.cs
@@ -31,6 +31,8 @@ public partial class DashCollectible : Node2D
         if (CollisionUtil.IsPlayer(body))
         {
             await _cutsceneManager.PlaySingleCharacterLine("Hink", "Holy shit!");
+            await _cutsceneManager.PlaySingleCharacterLine("Hink", "The dash orb! I found it!");
+            await _cutsceneManager.PlaySingleCharacterLine("Hink", "Maybe this thing can get me out of here.");
             _gameStateManager.GameState.PlayerState.PlayerSkillsState.CanDash.SetValue(true);
             QueueFree();
         }
