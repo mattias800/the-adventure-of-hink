0f60f024d0a0abd8bf76424ab70a2c5e4e8d8ca0
Mattias Andersson <mattias800@gmail.com>
2024-09-08 11:22:42 +0200

    Can now collect dash.

---
+++ game/collectibles/dash/DashCollectible.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+using Theadventureofhink.autoloads;
+using Theadventureofhink.game_state;
+using Theadventureofhink.utils;
+
+public partial class DashCollectible : Node2D
+{
+    private Area2D _area2d;
+    private GameStateManager _gameStateManager;
+    private CutsceneManager _cutsceneManager;
+
+    public override void _Ready()
+    {
+        _area2d = GetNode<Area2D>("Area2D");
+        _gameStateManager = GetNode<GameStateManager>(Singletons.GameStateManager);
+        _cutsceneManager = GetNode<CutsceneManager>(Singletons.CutsceneManager);
+
+        if (_gameStateManager.GameState.PlayerState.PlayerSkillsState.CanDash.Value)
+        {
+            QueueFree();
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+
+    public async void OnBodyEntered(Node2D body)
+    {
+        if (CollisionUtil.IsPlayer(body))
+        {
+            await _cutsceneManager.PlaySingleCharacterLine("Hink", "Holy shit!");
+            _gameStateManager.GameState.PlayerState.PlayerSkillsState.CanDash.SetValue(true);
+            QueueFree();
+        }
+    }
+}
